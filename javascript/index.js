2a9fe9e3df14a0f9624b073813883efc5124c17c
bhaawnaBM <bhaawna1805@gmail.com>
2023-02-02 15:19:00 +1100

    added functionality for select of cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,42 @@
 setInterval(function () {
-  // Indianapolis
-  let indianapolisDate = document.querySelector("#indianapolis .date");
-  let indianapolisTime = document.querySelector("#indianapolis .time");
-  indianapolisDate.innerHTML = moment().format("MMMM Do, YYYY");
-  indianapolisTime.innerHTML = moment()
-    .tz("America/Indiana/Indianapolis")
-    .format("h:mm:ss [<small>]A[</small>]");
-
+  // Melbourne
+  let melbourneElement = document.querySelector("#melbourne");
+  if (melbourneElement) {
+    let melbourneDate = document.querySelector("#melbourne .date");
+    let melbourneTime = document.querySelector("#melbourne .time");
+    melbourneDate.innerHTML = moment().format("MMMM Do, YYYY");
+    melbourneTime.innerHTML = moment()
+      .tz("Australia/Melbourne")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
   //Bangalore
-  let bangaloreDate = document.querySelector("#bangalore .date");
-  let bangaloreTime = document.querySelector("#bangalore .time");
-  bangaloreDate.innerHTML = moment().format("MMMM Do, YYYY");
-  bangaloreTime.innerHTML = moment()
-    .tz("Asia/Kolkata")
-    .format("h:mm:ss [<small>]A[</small>]");
+  let bangaloreElement = document.querySelector("#bangalore");
+  if (bangaloreElement) {
+    let bangaloreDate = document.querySelector("#bangalore .date");
+    let bangaloreTime = document.querySelector("#bangalore .time");
+    bangaloreDate.innerHTML = moment().format("MMMM Do, YYYY");
+    bangaloreTime.innerHTML = moment()
+      .tz("Asia/Kolkata")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
 }, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+     <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>`;
+}
+
+let citiesSelect = document.querySelector("#selectCity");
+citiesSelect.addEventListener("change", updateCity);
